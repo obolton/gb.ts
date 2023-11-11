3c8d34228dc36cf092d5baf856a5716c81a3dcfe
Oliver Bolton <obolton@gmail.com>
2023-11-11 12:37:08 -0800

    Support GBC graphics

--- src/graphics/ppu.ts
+++ src/graphics/ppu.ts
@@ -17,6 +17,14 @@ export enum Mode {
   RENDER,
 }
 
+type TileAttributes = {
+  priority: boolean;
+  flipY: boolean;
+  flipX: boolean;
+  palette: number;
+  data: number;
+};
+
 type ObjectAttributes = {
   address: number;
   x: number;
@@ -26,11 +34,20 @@ type ObjectAttributes = {
   flipX: boolean;
   flipY: boolean;
   backgroundPriority: boolean;
+  bank: number;
+  cgbPalette: number;
+};
+
+type PixelInfo = {
+  colorIndex: number;
+  color: number;
+  bgPriority: boolean;
 };
 
 /** Picture Processing Unit */
 export default class PPU {
   mmu?: MMU;
+  cgbMode: boolean = false;
 
   context: CanvasRenderingContext2D;
   imageData: ImageData;
@@ -39,7 +56,8 @@ export default class PPU {
   vramBank = 0;
 
   objects: ObjectAttributes[] = [];
-  backgroundPixels = new Array<number>(SCREEN_WIDTH);
+  backgroundPixels = new Array<PixelInfo>(SCREEN_WIDTH);
+  objectPixels = new Array<PixelInfo>(SCREEN_WIDTH);
   clock = 0;
   dma = 0xff;
 
@@ -64,6 +82,20 @@ export default class PPU {
   objectPalette0 = [0, 0, 0, 0];
   objectPalette1 = [0, 0, 0, 0];
 
+  // Color
+  colorBackgroundPalette = new Uint8Array(64).fill(255);
+  backgroundPaletteIndex = 0;
+  incrementBackgroundPaletteIndex = false;
+
+  colorObjectPalette = new Uint8Array(64).fill(255);
+  objectPaletteIndex = 0;
+  incrementObjectPaletteIndex = false;
+
+  vramDmaSource = 0x0000;
+  vramDmaDestination = 0x0000;
+  vramDmaLength = 0;
+  vramDmaProgress = 0;
+
   lycInterruptsEnabled = false;
   oamStatInterruptsEnabled = false;
   verticalBlankStatInterruptsEnabled = false;
@@ -86,7 +118,8 @@ export default class PPU {
     this.vramBank = 0;
 
     this.objects = [];
-    this.backgroundPixels = new Array<number>(SCREEN_WIDTH);
+    this.backgroundPixels = new Array<PixelInfo>(SCREEN_WIDTH);
+    this.objectPixels = new Array<PixelInfo>(SCREEN_WIDTH);
     this.clock = 0;
 
     this.backgroundWindowEnabled = false;
@@ -109,6 +142,19 @@ export default class PPU {
     this.backgroundPalette = [0, 0, 0, 0];
     this.objectPalette0 = [0, 0, 0, 0];
     this.objectPalette1 = [0, 0, 0, 0];
+
+    this.colorBackgroundPalette = new Uint8Array(64).fill(255);
+    this.backgroundPaletteIndex = 0;
+    this.incrementBackgroundPaletteIndex = false;
+
+    this.colorObjectPalette = new Uint8Array(64).fill(255);
+    this.objectPaletteIndex = 0;
+    this.incrementObjectPaletteIndex = false;
+
+    this.vramDmaSource = 0x0000;
+    this.vramDmaDestination = 0x0000;
+    this.vramDmaLength = 0;
+    this.vramDmaProgress = 0;
 
     this.lycInterruptsEnabled = false;
     this.oamStatInterruptsEnabled = false;
@@ -135,6 +181,7 @@ export default class PPU {
         );
       case GRAPHICS_REGISTERS.STAT:
         return (
+          0x80 |
           (this.lycInterruptsEnabled ? 0x40 : 0) |
           (this.oamStatInterruptsEnabled ? 0x20 : 0) |
           (this.verticalBlankStatInterruptsEnabled ? 0x10 : 0) |
@@ -177,6 +224,36 @@ export default class PPU {
         return this.wy;
       case GRAPHICS_REGISTERS.WX:
         return this.wx;
+      case GRAPHICS_REGISTERS.VBK:
+        return this.vramBank | 0xfe;
+      case GRAPHICS_REGISTERS.BCPS:
+        return (
+          (this.incrementBackgroundPaletteIndex ? 0x80 : 0) |
+          0x40 |
+          this.backgroundPaletteIndex
+        );
+      case GRAPHICS_REGISTERS.BCPD:
+        return this.colorBackgroundPalette[this.backgroundPaletteIndex];
+      case GRAPHICS_REGISTERS.OCPS:
+        return (
+          (this.incrementObjectPaletteIndex ? 0x80 : 0) |
+          0x40 |
+          this.objectPaletteIndex
+        );
+      case GRAPHICS_REGISTERS.OCPD:
+        return this.colorObjectPalette[this.objectPaletteIndex];
+      case GRAPHICS_REGISTERS.HDMA1:
+        return this.vramDmaSource >> 8;
+      case GRAPHICS_REGISTERS.HDMA2:
+        return this.vramDmaSource & 0x00ff;
+      case GRAPHICS_REGISTERS.HDMA3:
+        return this.vramDmaDestination >> 8;
+      case GRAPHICS_REGISTERS.HDMA4:
+        return this.vramDmaDestination & 0x00ff;
+      case GRAPHICS_REGISTERS.HDMA5:
+        return this.vramDmaProgress >= this.vramDmaLength
+          ? 0xff
+          : this.vramDmaLength - this.vramDmaProgress;
       default:
         return 0xff;
     }
@@ -247,6 +324,46 @@ export default class PPU {
         return;
       case GRAPHICS_REGISTERS.WX:
         this.wx = value;
+        return;
+      case GRAPHICS_REGISTERS.VBK:
+        this.vramBank = value;
+        return;
+      case GRAPHICS_REGISTERS.BCPS:
+        this.backgroundPaletteIndex = value & 0x3f;
+        this.incrementBackgroundPaletteIndex = Boolean(value & 0x80);
+        return;
+      case GRAPHICS_REGISTERS.BCPD:
+        this.colorBackgroundPalette[this.backgroundPaletteIndex] = value;
+        if (this.incrementBackgroundPaletteIndex) {
+          this.backgroundPaletteIndex =
+            (this.backgroundPaletteIndex + 1) & 0x3f;
+        }
+        return;
+      case GRAPHICS_REGISTERS.OCPS:
+        this.objectPaletteIndex = value & 0x3f;
+        this.incrementObjectPaletteIndex = Boolean(value & 0x80);
+        return;
+      case GRAPHICS_REGISTERS.OCPD:
+        this.colorObjectPalette[this.objectPaletteIndex] = value;
+        if (this.incrementObjectPaletteIndex) {
+          this.objectPaletteIndex = (this.objectPaletteIndex + 1) & 0x3f;
+        }
+        return;
+      case GRAPHICS_REGISTERS.HDMA1:
+        this.vramDmaSource = (value << 8) | (this.vramDmaSource & 0x00ff);
+        return;
+      case GRAPHICS_REGISTERS.HDMA2:
+        this.vramDmaSource = (this.vramDmaSource & 0xff00) | value;
+        return;
+      case GRAPHICS_REGISTERS.HDMA3:
+        this.vramDmaDestination =
+          (value << 8) | (this.vramDmaDestination & 0x00ff);
+        return;
+      case GRAPHICS_REGISTERS.HDMA4:
+        this.vramDmaDestination = (this.vramDmaDestination & 0xff00) | value;
+        return;
+      case GRAPHICS_REGISTERS.HDMA5:
+        this.vramDma((value & 0x80) >> 7, value & 0x7f);
         return;
     }
   }
@@ -319,6 +436,10 @@ export default class PPU {
   horizontalBlankMode() {
     this.mode = Mode.HORIZONTAL_BLANK;
 
+    if (this.cgbMode && this.vramDmaProgress < this.vramDmaLength) {
+      this.hblankDma();
+    }
+
     if (this.horizontalBlankStatInterruptsEnabled) {
       this.mmu?.requestInterrupt(Interrupts.LCD_STAT);
     }
@@ -364,18 +485,30 @@ export default class PPU {
     }
   }
 
-  renderScanline() {
-    if (this.backgroundWindowEnabled) {
-      this.drawBackground();
+  getPixel(x: number) {
+    const bgPixel = this.backgroundPixels[x];
+    const objectPixel = this.objectPixels[x];
 
-      if (
-        this.windowEnabled &&
-        this.wx < SCREEN_WIDTH + 7 &&
-        this.wy < SCREEN_HEIGHT &&
-        this.ly >= this.wy
-      ) {
-        this.drawWindow();
-      }
+    if (!objectPixel) {
+      return bgPixel;
+    }
+
+    if (
+      bgPixel.colorIndex === 0 ||
+      !this.backgroundWindowEnabled ||
+      (!bgPixel.bgPriority && !objectPixel.bgPriority)
+    ) {
+      return objectPixel;
+    }
+
+    return bgPixel;
+  }
+
+  renderScanline() {
+    this.backgroundPixels = [];
+    this.objectPixels = [];
+    if (this.backgroundWindowEnabled || this.cgbMode) {
+      this.drawLine();
     } else {
       this.drawBlankScanline();
     }
@@ -383,9 +516,19 @@ export default class PPU {
     if (this.objectsEnabled) {
       this.drawObjects();
     }
+
+    for (let x = 0; x < SCREEN_WIDTH; x++) {
+      const pixel = this.getPixel(x);
+
+      if (this.cgbMode) {
+        this.drawColorPixel(x, this.ly, pixel.color);
+      } else {
+        this.drawMonochromePixel(x, this.ly, pixel.color);
+      }
+    }
   }
 
-  drawPixel(x: number, y: number, color: number) {
+  drawMonochromePixel(x: number, y: number, color: number) {
     const offset = (y * SCREEN_WIDTH + x) * 4;
     const value = COLOR_MAP[color];
     this.imageData.data[offset] = value;
@@ -394,10 +537,24 @@ export default class PPU {
     this.imageData.data[offset + 3] = 255;
   }
 
+  drawColorPixel(x: number, y: number, color: number) {
+    const offset = (y * SCREEN_WIDTH + x) * 4;
+    const red = color & 0x001f;
+    const green = (color >> 5) & 0x001f;
+    const blue = (color >> 10) & 0x001f;
+    this.imageData.data[offset] = (red << 3) | (red >> 2);
+    this.imageData.data[offset + 1] = (green << 3) | (green >> 2);
+    this.imageData.data[offset + 2] = (blue << 3) | (blue >> 2);
+    this.imageData.data[offset + 3] = 255;
+  }
+
   drawBlankScanline() {
     for (let x = 0; x < SCREEN_WIDTH; x++) {
-      this.backgroundPixels[x] = 0;
-      this.drawPixel(x, this.ly, 0);
+      this.backgroundPixels[x] = {
+        color: this.cgbMode ? 0xffff : 0,
+        colorIndex: 0,
+        bgPriority: false,
+      };
     }
   }
 
@@ -417,7 +574,7 @@ export default class PPU {
     row: number,
     rowInTile: number,
     max: number
-  ) {
+  ): TileAttributes[] {
     if (!this.mmu) {
       return [];
     }
@@ -425,8 +582,41 @@ export default class PPU {
     for (let i = 0; i < max; i++) {
       const mapAddress = tileMapAddress + row * 32 + i;
       const tileDataIndex = this.mmu.read(mapAddress);
-      const address = this.getTileAddress(tileDataIndex);
-      tiles[i] = this.mmu.readWord(address + rowInTile * 2);
+
+      if (this.cgbMode) {
+        const tileAttributes = this.vramRead(
+          mapAddress - MEMORY_RANGES.VRAM.start,
+          1
+        );
+
+        const bank = (tileAttributes & 0x08) >> 3;
+        const flipY = Boolean(tileAttributes & 0x40);
+
+        const address =
+          this.getTileAddress(tileDataIndex) +
+          (flipY ? 7 - rowInTile : rowInTile) * 2 -
+          MEMORY_RANGES.VRAM.start;
+
+        tiles[i] = {
+          priority: Boolean(tileAttributes & 0x80),
+          flipY: Boolean(tileAttributes & 0x40),
+          flipX: Boolean(tileAttributes & 0x20),
+          palette: tileAttributes & 0x07,
+          data: this.vramReadWord(address, bank),
+        };
+      } else {
+        const address =
+          this.getTileAddress(tileDataIndex) +
+          rowInTile * 2 -
+          MEMORY_RANGES.VRAM.start;
+        tiles[i] = {
+          priority: false,
+          flipY: false,
+          flipX: false,
+          palette: 0,
+          data: this.vramReadWord(address, 0),
+        };
+      }
     }
     return tiles;
   }
@@ -437,39 +627,65 @@ export default class PPU {
     return ((low >> (7 - x)) & 1) + (((high >> (7 - x)) & 1) << 1);
   }
 
-  drawBackground() {
-    if (!this.mmu) {
-      return;
-    }
-
-    const y = (this.ly + this.scy) % 256;
-    const tileMap = this.backgroundTileMapAreaFlag ? 0x9c00 : 0x9800;
-    const tiles = this.getTiles(tileMap, y >> 3, y % 8, 32);
-
-    for (let displayX = 0; displayX < SCREEN_WIDTH; displayX++) {
-      const x = (this.scx + displayX) % 256;
-      const pixel = this.getTilePixel(tiles[x >> 3], x % 8);
-      this.backgroundPixels[displayX] = pixel;
-      this.drawPixel(displayX, this.ly, this.backgroundPalette[pixel]);
-    }
+  getBackgroundColor(palette: number, colorIndex: number) {
+    const offset = palette * 8 + colorIndex * 2;
+    return (
+      (this.colorBackgroundPalette[offset + 1] << 8) +
+      this.colorBackgroundPalette[offset]
+    );
   }
 
-  drawWindow() {
-    if (!this.mmu) {
-      return;
+  getObjectColor(palette: number, colorIndex: number) {
+    const offset = palette * 8 + colorIndex * 2;
+    return (
+      (this.colorObjectPalette[offset + 1] << 8) +
+      this.colorObjectPalette[offset]
+    );
+  }
+
+  drawLine() {
+    const bgY = (this.ly + this.scy) % 256;
+    const bgTileMap = this.backgroundTileMapAreaFlag ? 0x9c00 : 0x9800;
+    const bgTiles = this.getTiles(bgTileMap, bgY >> 3, bgY % 8, 32);
+
+    const drawWindow =
+      this.windowEnabled &&
+      this.wx < SCREEN_WIDTH + 7 &&
+      this.wy < SCREEN_HEIGHT &&
+      this.ly >= this.wy;
+
+    const windowTileMap = this.windowTileMapAreaFlag ? 0x9c00 : 0x9800;
+    const windowTiles = drawWindow
+      ? this.getTiles(windowTileMap, this.wly >> 3, this.wly % 8, 20)
+      : [];
+
+    for (let x = 0; x < SCREEN_WIDTH; x++) {
+      const bgX = (this.scx + x) & 255;
+      const windowX = x - (this.wx - 7);
+      const useWindowTile = drawWindow && windowX >= 0;
+
+      const tile = useWindowTile
+        ? windowTiles[windowX >> 3]
+        : bgTiles[bgX >> 3];
+
+      const tilePixelX = (useWindowTile ? windowX : bgX) % 8;
+
+      const color = this.getTilePixel(
+        tile.data,
+        tile.flipX ? 7 - tilePixelX : tilePixelX
+      );
+      this.backgroundPixels[x] = {
+        colorIndex: color,
+        color: this.cgbMode
+          ? this.getBackgroundColor(tile.palette, color)
+          : this.backgroundPalette[color],
+        bgPriority: tile.priority,
+      };
     }
 
-    const tileMap = this.windowTileMapAreaFlag ? 0x9c00 : 0x9800;
-    const tiles = this.getTiles(tileMap, this.wly >> 3, this.wly % 8, 20);
-
-    for (let i = Math.max(this.wx - 7, 0); i < SCREEN_WIDTH; i++) {
-      const x = i - this.wx + 7;
-      const pixel = this.getTilePixel(tiles[x >> 3], x % 8);
-      this.backgroundPixels[i] = pixel;
-      this.drawPixel(i, this.ly, this.backgroundPalette[pixel]);
+    if (drawWindow) {
+      this.wly++;
     }
-
-    this.wly++;
   }
 
   getObjects(): ObjectAttributes[] {
@@ -493,6 +709,8 @@ export default class PPU {
           y,
           x,
           tile,
+          cgbPalette: attributes & 0x07,
+          bank: (attributes & 0x08) >> 3,
           paletteFlag: Boolean(attributes & 0x10),
           flipX: Boolean(attributes & 0x20),
           flipY: Boolean(attributes & 0x40),
@@ -502,16 +720,13 @@ export default class PPU {
         objects.push(object);
       }
     }
-    objects.sort((a, b) => (a.x === b.x ? a.address - b.address : a.x - b.x));
+    objects.sort((a, b) =>
+      a.x === b.x || this.cgbMode ? a.address - b.address : a.x - b.x
+    );
     return objects;
   }
 
   drawObjects() {
-    if (!this.mmu) {
-      return;
-    }
-
-    const objectPixels: number[] = [];
     const objectSize = this.objectSizeFlag ? 16 : 8;
 
     for (let j = 0; j < this.objects.length; j++) {
@@ -533,8 +748,9 @@ export default class PPU {
         }
       }
 
-      const tileAddress = 0x8000 + tileIndex * 16;
-      const tileData = this.mmu.readWord(tileAddress + (tileY % 8) * 2);
+      const bank = this.cgbMode ? object.bank : 0;
+      const tileAddress = tileIndex * 16 + (tileY % 8) * 2;
+      const tileData = this.vramReadWord(tileAddress, bank);
 
       for (let k = 0; k < 8; k++) {
         const x = object.x + k;
@@ -543,21 +759,69 @@ export default class PPU {
           continue;
         }
 
-        if (typeof objectPixels[x] !== 'undefined') {
+        if (typeof this.objectPixels[x] !== 'undefined') {
           continue;
         }
 
-        if (object.backgroundPriority && this.backgroundPixels[x] !== 0) {
+        const colorIndex = this.getTilePixel(
+          tileData,
+          object.flipX ? 7 - k : k
+        );
+
+        if (colorIndex === 0) {
           continue;
         }
 
-        const pixel = this.getTilePixel(tileData, object.flipX ? 7 - k : k);
-        objectPixels[x] = pixel;
-
-        if (pixel > 0) {
-          this.drawPixel(x, this.ly, palette[pixel]);
-        }
+        this.objectPixels[x] = {
+          colorIndex,
+          color: this.cgbMode
+            ? this.getObjectColor(object.cgbPalette, colorIndex)
+            : palette[colorIndex],
+          bgPriority: object.backgroundPriority,
+        };
       }
     }
+  }
+
+  vramDma(mode: number, length: number) {
+    if (mode === 0) {
+      this.generalPurposeDma(length);
+    } else {
+      this.vramDmaProgress = 0;
+      this.vramDmaLength = length + 1;
+    }
+  }
+
+  generalPurposeDma(length: number) {
+    if (!this.mmu) {
+      return;
+    }
+    const transferLength = (length + 1) * 0x10;
+    const source = this.vramDmaSource & 0xfff0;
+    const destination = this.vramDmaDestination & 0x1ff0;
+    for (let i = 0; i < transferLength; i++) {
+      this.vramWrite(destination + i, this.mmu.read(source + i));
+    }
+    this.vramDmaProgress = 0;
+    this.vramDmaLength = 0;
+  }
+
+  hblankDma() {
+    if (!this.mmu) {
+      return;
+    }
+    const offset = this.vramDmaProgress * 0x10;
+    const source = (this.vramDmaSource & 0xfff0) + offset;
+    const destination = (this.vramDmaDestination & 0x1ff0) + offset;
+
+    for (let i = 0; i < 0x10; i++) {
+      // Overflow
+      if (source + i > 0xffff || destination + i > 0xffff) {
+        return;
+      }
+      this.vramWrite(destination + i, this.mmu.read(source + i));
+    }
+
+    this.vramDmaProgress++;
   }
 }
