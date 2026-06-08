659419bfa4be9b6ce919254ce91bf396bfd8efd2
Oliver Bolton <obolton@gmail.com>
2026-06-08 11:59:08 -0400

    Clean up

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -24,109 +24,81 @@ const KEY_MAPPING: { [code: string]: Button } = {
   Enter: Button.START,
 };
 
-(function init() {
-  const display = document.getElementById('display') as HTMLCanvasElement;
-  const fileInput = document.getElementById('romSelectorInput') as HTMLInputElement;
-  const device = document.getElementById('device') as HTMLDivElement;
-  const powerButton = document.getElementById('powerButton') as HTMLButtonElement;
-  const controls = document.getElementById('controls') as HTMLDivElement;
+const display = document.getElementById('display') as HTMLCanvasElement;
+const fileInput = document.getElementById('romSelectorInput') as HTMLInputElement;
+const device = document.getElementById('device') as HTMLDivElement;
+const powerButton = document.getElementById('powerButton') as HTMLButtonElement;
 
-  const gameboy = new GameBoy(display);
+const gameboy = new GameBoy(display);
 
-  function enableAudio() {
-    gameboy.apu.resume().then(() => {
-      window.removeEventListener('pointerdown', enableAudio);
-      window.removeEventListener('keydown', enableAudio);
-    });
+function enableAudio() {
+  gameboy.apu.resume().then(() => {
+    window.removeEventListener('pointerdown', enableAudio);
+    window.removeEventListener('keydown', enableAudio);
+  });
+}
+window.addEventListener('pointerdown', enableAudio);
+window.addEventListener('keydown', enableAudio);
+
+// Bind button events
+Object.entries(BUTTON_MAPPING).forEach(([id, button]) => {
+  const element = document.getElementById(id);
+  if (!element) {
+    return;
   }
-  window.addEventListener('pointerdown', enableAudio);
-  window.addEventListener('keydown', enableAudio);
+  element.addEventListener('pointerdown', () => gameboy.input.buttonDown(button));
+  element.addEventListener('pointerup', () => gameboy.input.buttonUp(button));
+});
 
-  // Bind button events
-  Object.entries(BUTTON_MAPPING).forEach(([id, button]) => {
-    const element = document.getElementById(id);
-    if (!element) {
-      return;
-    }
-    element.addEventListener('mousedown', (event) => {
-      gameboy.input.buttonDown(button);
-      event.preventDefault();
-      event.stopPropagation();
-    });
-    element.addEventListener('touchstart', (event) => {
-      gameboy.input.buttonDown(button);
-      event.preventDefault();
-      event.stopPropagation();
-    });
-    element.addEventListener('mouseup', (event) => {
-      gameboy.input.buttonUp(button);
-      event.preventDefault();
-      event.stopPropagation();
-    });
-    element.addEventListener('touchend', (event) => {
-      gameboy.input.buttonUp(button);
-      event.preventDefault();
-      event.stopPropagation();
-    });
-  });
+powerButton.addEventListener('click', () => {
+  gameboy.stop();
+  display.getContext('2d')?.clearRect(0, 0, display.width, display.height);
+  device.classList.remove('on');
+  powerButton.disabled = true;
+  document.title = 'gb.ts';
+});
 
-  // Disable other touch events
-  controls.addEventListener('touchstart', (event) => {
+// Bind key events
+window.addEventListener('keydown', (event: KeyboardEvent) => {
+  const button = KEY_MAPPING[event.code];
+  if (button) {
+    gameboy.input.buttonDown(button);
     event.preventDefault();
-  });
+  }
+});
+window.addEventListener('keyup', (event: KeyboardEvent) => {
+  const button = KEY_MAPPING[event.code];
+  if (button) {
+    gameboy.input.buttonUp(button);
+    event.preventDefault();
+  }
+});
 
-  powerButton.addEventListener('click', () => {
-    gameboy.stop();
-    display.getContext('2d')?.clearRect(0, 0, display.width, display.height);
-    device.classList.remove('on');
-    powerButton.disabled = true;
-    document.title = 'gb.ts';
-  });
-
-  // Bind key events
-  window.addEventListener('keydown', (event: KeyboardEvent) => {
-    const button = KEY_MAPPING[event.code];
-    if (typeof button !== 'undefined') {
-      gameboy.input.buttonDown(button);
-      event.preventDefault();
-      event.stopPropagation();
+// File selection
+const reader = new FileReader();
+reader.addEventListener('load', () => {
+  const result = reader.result;
+  if (!(result instanceof ArrayBuffer)) {
+    throw new Error('Invalid file');
+  }
+  device.classList.add('on');
+  powerButton.disabled = false;
+  const rom = new Uint8Array(result);
+  try {
+    gameboy.start(rom);
+    document.title = `gb.ts - ${gameboy.getGameTitle()}`;
+  } catch (error) {
+    if (error instanceof Error) {
+      window.alert(error.message);
+      device.classList.remove('on');
+      powerButton.disabled = true;
     }
-  });
-  window.addEventListener('keyup', (event: KeyboardEvent) => {
-    const button = KEY_MAPPING[event.code];
-    if (typeof button !== 'undefined') {
-      gameboy.input.buttonUp(button);
-      event.preventDefault();
-      event.stopPropagation();
-    }
-  });
-
-  // File selection
-  const reader = new FileReader();
-  reader.addEventListener('load', () => {
-    const result = reader.result;
-    if (!(result instanceof ArrayBuffer)) {
-      throw new Error('Invalid file');
-    }
-    device.classList.add('on');
-    powerButton.disabled = false;
-    const rom = new Uint8Array(result);
-    try {
-      gameboy.start(rom);
-      document.title = `gb.ts - ${gameboy.getGameTitle()}`;
-    } catch (error) {
-      if (error instanceof Error) {
-        window.alert(error.message);
-        device.classList.remove('on');
-        powerButton.disabled = true;
-      }
-    }
-  });
-  fileInput.addEventListener('change', () => {
-    const file = fileInput.files?.[0];
-    if (file) {
-      reader.readAsArrayBuffer(file);
-      fileInput.value = '';
-    }
-  });
-})();
+  }
+});
+fileInput.addEventListener('change', () => {
+  const file = fileInput.files?.[0];
+  if (file) {
+    reader.readAsArrayBuffer(file);
+    fileInput.value = '';
+  }
+});
